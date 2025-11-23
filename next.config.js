2781d4c11afc7e1a48e96f3797b8225231186f8e
codewithwahib <wahibjaffer043@gmail.com>
2025-11-23 14:58:05 +0500

    e

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['cdn.sanity.io'],
-    // or use remotePatterns for more control (recommended for newer Next.js versions)
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'cdn.sanity.io',
         port: '',
-        pathname: '/images/**',
+        pathname: '/**', // This allows all paths from cdn.sanity.io
       },
     ],
   },
